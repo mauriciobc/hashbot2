e5a9b81d4c78eeb73b65039a878214621a8e6e6d
Mauricio Castro <mauriciobarbosaecastro@outlook.com>
2024-07-27 12:12:00 -0300

    Initial commit

--- config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const CLIENT_KEY = process.env.CLIENT_KEY;
-export const CLIENT_SECRET = process.env.CLIENT_SECRET;
-export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-export const MASTODON_URL = process.env.MASTODON_URL;
-export const PREFERRED_TIMEZONE = 'America/Sao_Paulo';
